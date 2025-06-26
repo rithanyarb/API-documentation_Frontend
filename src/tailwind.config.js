0111f25ba5393e6e182f27af1bcbfce53977b1fe
rithanyarb <rbrithanya003@gmail.com>
2025-06-26 16:09:16 +0530

    tailwind update

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,6 +1,5 @@
-// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
-//vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
 });
